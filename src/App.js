172642dc568c72114dcde0cc34f1eecd32082ee8
n0235026 <ethan.olson@libertymutual.com>
2019-09-24 16:57:58 -0500

    Add react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { Header } from './Header';
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 
 function App() {
   return (
-      //Header
-      <Header/>
+      <Router>
+        <Header/>
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+      </Router>
       //Body
       //Footer
   );
--- src/Header/components/Header.js
+++ src/Header/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {Link } from "react-router-dom";
+
 
 const Header = () => (
-    <div>This is the Header</div>
+    <React.Fragment>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about/">About</Link>
+                </li>
+            </ul>
+        </nav>
+    </React.Fragment>
 );
 
 export default Header;
