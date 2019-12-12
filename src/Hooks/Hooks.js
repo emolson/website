25a170435343f9e069586be59e614d127f2496ae
n0235026 <ethan.olson@libertymutual.com>
2019-12-12 11:47:56 -0800

    added useEffect

--- src/Hooks/Hooks.js
+++ src/Hooks/Hooks.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const Hooks = () => (
-    <div>Inside Hooks</div>
-);
+const Hooks = () => {
+    //useStates returns and array with two elements [currentState, overRideFunc()]
+    const [count, setCount] = useState(0);
+    const [countVisibility, setCountVisibility] = useState(true);
+
+    //useEffect takes 2 arguments
+    // 1) function, this fnc will be executed after every component rendering
+    // 2) array of dependencies that if changed, rerun useEffect
+    useEffect(() => {
+        console.log("Hooks component mounted");
+    }, [count]);
+
+    const countIncrement = () => {
+        setCount(count+1);
+    };
+
+    const countDecrement = () => {
+        setCount(count-1);
+    };
+
+    const changeVisibility = () => {
+        setCountVisibility(!countVisibility);
+    };
+
+
+    return (
+        <>
+            <div>Inside Hooks</div>
+            <div hidden={!countVisibility}>{count}</div>
+            <button onClick={countIncrement}>Inc</button>
+            <button onClick={countDecrement}>Dec</button>
+            <button onClick={changeVisibility}>Change Visibility</button>
+        </>
+    )
+};
 
 
 export default Hooks;
